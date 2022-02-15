9730c7c54bef4d2cdbf5bed6d555ed5d1d5a5583
howlwindy <howlwindy@gmail.com>
2022-02-15 10:11:44 +0800

    feat(highlight): init

---
+++ src/pages/arrays/Arrays.vue
@@ -0,0 +1,6 @@
+<script setup lang="ts"></script>
+<template>
+  <h1>arrays</h1>
+  <router-view></router-view>
+</template>
+<style scoped></style>
---
+++ src/pages/arrays/statistics/Statistics.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import * as d3 from 'd3'
+
+// const min = d3.min()
+</script>
+<template>
+  <h4>statistics</h4>
+  <pre v-highlight><code class="typescript">{{ d3.min }}</code></pre>
+</template>
+<style scoped></style>
