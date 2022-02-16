4eb7893fc5b02b17d0d29e7bfbf5c1edd45cea96
howlwindy <howlwindy@gmail.com>
2022-02-16 09:47:21 +0800

    feat(statistics): update

--- src/pages/arrays/statistics/Statistics.vue
+++ src/pages/arrays/statistics/Statistics.vue
@@ -1,10 +1,37 @@
 <script setup lang="ts">
 import * as d3 from 'd3'
+import { STATISTICS_SRC } from './src.data'
 
-// const min = d3.min()
+const DATA = ['1', 'a']
+const min = [`d3.min(DATA) => ${d3.min(DATA)}`]
+// const minIndex = d3.minIndex()
+// const max = d3.max()
+// const maxIndex = d3.maxIndex()
+// const extent = d3.extent()
+// const sum = d3.sum()
+// const mean = d3.mean()
+// const median = d3.median()
+// const mode = d3.mode()
+// const cumsum = d3.cumsum()
+// const rank = d3.rank()
+// const quantile = d3.quantile()
+// const quantileSorted = d3.quantileSorted()
+// const deviation = d3.deviation()
+// const fcumsum = d3.fcumsum()
+// const fsum = d3.fsum()
+// const adder = new d3.Adder()
+// const add = adder.add()
+// const valueOf = adder.valueOf()
 </script>
 <template>
   <h4>statistics</h4>
-  <pre v-highlight><code class="typescript">{{ d3.min }}</code></pre>
+  <pre><code>DATA = {{ DATA }}</code></pre>
+  <pre><code>{{ min[0] }}</code></pre>
+  <pre
+    v-for="(v, k) of STATISTICS_SRC"
+    :key="k"
+    v-highlight
+    style="padding-right: 5px"
+  ><code class="typescript" style="overflow-x: scroll">{{ v.join('\n') }}</code></pre>
 </template>
 <style scoped></style>
