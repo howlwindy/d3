6191f80c1ff7ae697c6de33cb7dd28bc390d6950
howlwindy <howlwindy@gmail.com>
2022-02-22 20:38:33 +0800

    feat(arrays): statistics
    median

--- src/models/arrays/statistics/Mean.vue
+++ src/models/arrays/statistics/Mean.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { ref } from 'vue'
-import { strToStrArr, strToNumArr } from '@/common/utils/toArray'
+import { strToNumArr } from '@/common/utils/toArray'
 import SrcVue from '@/components/Src.vue'
 import EgVue from '@/components/Eg.vue'
 import CodeVue from '@/components/Code.vue'
@@ -14,13 +14,13 @@ const src = ref<string[]>([
 
 const data0 = ref<string>('3 2 10 -1 -2')
 const result0 = ref<number | undefined>(d3.mean(strToNumArr(data0.value)))
-const input1 = (v: string) => (result0.value = d3.mean(strToNumArr(v)))
+const input0 = (v: string) => (result0.value = d3.mean(strToNumArr(v)))
 
 const data1 = ref<string>('a 2 10 -1 -2')
 const result1 = ref<number | undefined>(
   d3.mean(strToNumArr(data1.value), (datum) => datum)
 )
-const input2 = (v: string) =>
+const input1 = (v: string) =>
   (result1.value = d3.mean(strToNumArr(v), (datum) => datum))
 </script>
 <template>
@@ -28,18 +28,15 @@ const input2 = (v: string) =>
     <h3><code>mean</code></h3>
     <SrcVue :data="src"></SrcVue>
     <EgVue>
-      <input v-model="data0" type="text" @input="input1(data0)" />
+      <input v-model="data0" type="text" @input="input0(data0)" />
       <CodeVue :result="result0">
         <code>d3.mean({{ strToNumArr(data0) }})</code>
       </CodeVue>
     </EgVue>
     <EgVue>
-      <input v-model="data1" type="text" @input="input2(data1)" />
+      <input v-model="data1" type="text" @input="input1(data1)" />
       <CodeVue :result="result1">
-        <code>
-          d3.mean({{ strToStrArr(data1) }}, (datum, index, array) => `${datum} -
-          ${index} - ${array}`)
-        </code>
+        <code>d3.mean({{ strToNumArr(data1) }}, (datum) => datum</code>
       </CodeVue>
     </EgVue>
   </GroupItemVue>
