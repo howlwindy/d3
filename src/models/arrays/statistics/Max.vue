8120bc16f9ba016cca1a9333225eaed76b0cd569
howlwindy <howlwindy@gmail.com>
2022-02-22 19:31:27 +0800

    feat(max maxindex): update

--- src/models/arrays/statistics/Max.vue
+++ src/models/arrays/statistics/Max.vue
@@ -1,9 +1,64 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+import { strToStrArr, strToNumArr } from '@/common/utils/toArray'
+import SrcVue from '@/components/Src.vue'
+import EgVue from '@/components/Eg.vue'
+import CodeVue from '@/components/Code.vue'
 import GroupItemVue from '@/components/GroupItem.vue'
+import * as d3 from 'd3'
+
+const src = ref<string[]>([
+  'max(iterable: Iterable<string>): string | undefined',
+  'max<T extends Numeric>(iterable: Iterable<T>): T | undefined',
+  'max<T>(iterable: Iterable<T>, accessor: (datum: T, index: number, array: Iterable<T>) => string | undefined | null): string | undefined'
+])
+
+const data0 = ref<string>('a 2 10 -1 -2')
+const result0 = ref<string | undefined>(d3.max(strToStrArr(data0.value)))
+const input0 = (v: string) => (result0.value = d3.max(strToStrArr(v)))
+
+const data1 = ref<string>('3 2 10 -1 -2')
+const result1 = ref<number | undefined>(d3.max(strToNumArr(data1.value)))
+const input1 = (v: string) => (result1.value = d3.max(strToNumArr(v)))
+
+const data2 = ref<string>('a 2 10 -1 -2')
+const result2 = ref<string | undefined>(
+  d3.max(
+    strToStrArr(data2.value),
+    (datum, index, array) => `${datum} - ${index} - ${array}`
+  )
+)
+const input2 = (v: string) =>
+  (result2.value = d3.max(
+    strToStrArr(v),
+    (datum, index, array) => `${datum} - ${index} - ${array}`
+  ))
 </script>
 <template>
   <GroupItemVue>
     <h3><code>max</code></h3>
+    <SrcVue :data="src"></SrcVue>
+    <EgVue>
+      <input v-model="data0" type="text" @input="input0(data0)" />
+      <CodeVue :result="result0">
+        <code>d3.max({{ strToStrArr(data0) }})</code>
+      </CodeVue>
+    </EgVue>
+    <EgVue>
+      <input v-model="data1" type="text" @input="input1(data1)" />
+      <CodeVue :result="result1">
+        <code>d3.max({{ strToNumArr(data1) }})</code>
+      </CodeVue>
+    </EgVue>
+    <EgVue>
+      <input v-model="data2" type="text" @input="input2(data2)" />
+      <CodeVue :result="result2">
+        <code>
+          d3.max({{ strToStrArr(data2) }}, (datum, index, array) => `${datum} -
+          ${index} - ${array}`)
+        </code>
+      </CodeVue>
+    </EgVue>
   </GroupItemVue>
 </template>
 <style scoped></style>
--- src/models/arrays/statistics/MaxIndex.vue
+++ src/models/arrays/statistics/MaxIndex.vue
@@ -1,9 +1,53 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+import { strToStrArr } from '@/common/utils/toArray'
+import SrcVue from '@/components/Src.vue'
+import EgVue from '@/components/Eg.vue'
+import CodeVue from '@/components/Code.vue'
 import GroupItemVue from '@/components/GroupItem.vue'
+import * as d3 from 'd3'
+
+const src = ref<string[]>([
+  'maxIndex(iterable: Iterable<unknown>): number',
+  'maxIndex<TDatum>(iterable: Iterable<TDatum>, accessor: (datum: TDatum, index: number, array: Iterable<TDatum>) => unknown): number'
+])
+
+const data0 = ref<string>('a 2 10 -1 -2')
+const result0 = ref<number | undefined>(d3.maxIndex(strToStrArr(data0.value)))
+const input0 = (v: string) => (result0.value = d3.maxIndex(strToStrArr(v)))
+
+const data1 = ref<string>('a 2 10 -1 -2')
+const result1 = ref<number | undefined>(
+  d3.maxIndex(
+    strToStrArr(data1.value),
+    (datum, index, array) => `${datum} - ${index} - ${array}`
+  )
+)
+const input1 = (v: string) =>
+  (result1.value = d3.maxIndex(
+    strToStrArr(v),
+    (datum, index, array) => `${datum} - ${index} - ${array}`
+  ))
 </script>
 <template>
   <GroupItemVue>
     <h3><code>maxIndex</code></h3>
+    <SrcVue :data="src"></SrcVue>
+    <EgVue>
+      <input v-model="data0" type="text" @input="input0(data0)" />
+      <CodeVue :result="result0">
+        <code>d3.maxIndex({{ strToStrArr(data0) }})</code>
+      </CodeVue>
+    </EgVue>
+    <EgVue>
+      <input v-model="data1" type="text" @input="input1(data1)" />
+      <CodeVue :result="result1">
+        <code>
+          d3.maxIndex({{ strToStrArr(data1) }}, (datum, index, array) =>
+          `${datum} - ${index} - ${array}`)
+        </code>
+      </CodeVue>
+    </EgVue>
   </GroupItemVue>
 </template>
 <style scoped></style>
