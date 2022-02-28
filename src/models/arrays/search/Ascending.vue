a46f1264704b0e0fad4febe08c61eaedbb5e5646
howlwindy <howlwindy@gmail.com>
2022-02-28 21:42:47 +0800

    feat(arrays): search
    ascending

--- src/models/arrays/search/Ascending.vue
+++ src/models/arrays/search/Ascending.vue
@@ -1,9 +1,30 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+import SrcVue from '@/components/Src.vue'
+import EgVue from '@/components/Eg.vue'
+import CodeVue from '@/components/Code.vue'
 import GroupItemVue from '@/components/GroupItem.vue'
+import * as d3 from 'd3'
+
+const src = ref<string[]>([
+  'ascending(a: Primitive | undefined, b: Primitive | undefined): number'
+])
+
+const data0 = ref<string>('a 2 10 -1 -2')
+const result0 = ref<string[]>(data0.value.split(' ').sort(d3.ascending))
+const input0 = (v0: string) =>
+  (result0.value = v0.split(' ').sort(d3.ascending))
 </script>
 <template>
   <GroupItemVue>
-    <h3><code>ascending</code></h3>
+    <h3><code>ascending - 升序</code></h3>
+    <SrcVue :data="src"></SrcVue>
+    <EgVue>
+      <input v-model="data0" type="text" @input="input0(data0)" />
+      <CodeVue :result="result0">
+        <code>{{ data0 }}.sort(d3.ascending)</code>
+      </CodeVue>
+    </EgVue>
   </GroupItemVue>
 </template>
 <style scoped></style>
