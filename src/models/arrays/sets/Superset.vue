bb69787f42c05ad2090d185cf4c2dadfd4245ee3
howlwindy <howlwindy@gmail.com>
2022-03-02 21:58:26 +0800

    feat(arrays): sets
    superset

--- src/models/arrays/sets/Superset.vue
+++ src/models/arrays/sets/Superset.vue
@@ -1,9 +1,29 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+import SrcVue from '@/components/Src.vue'
+import EgVue from '@/components/Eg.vue'
+import CodeVue from '@/components/Code.vue'
 import GroupItemVue from '@/components/GroupItem.vue'
+import * as d3 from 'd3'
+
+const src = ref<string[]>([
+  'superset<T>(a: Iterable<T>, b: Iterable<T>): boolean'
+])
+
+const data0 = ref<string>('a 2 10 -1 -2')
+const result0 = ref<boolean>(d3.superset(data0.value, '*'))
+const input0 = (v: string) => (result0.value = d3.superset(v, '*'))
 </script>
 <template>
   <GroupItemVue>
-    <h3><code>superset</code></h3>
+    <h3><code>superset - 断言前是后的超集</code></h3>
+    <SrcVue :data="src"></SrcVue>
+    <EgVue>
+      <input v-model="data0" type="text" @input="input0(data0)" />
+      <CodeVue :result="result0">
+        <code>d3.superset({{ data0 }}, '*')</code>
+      </CodeVue>
+    </EgVue>
   </GroupItemVue>
 </template>
 <style scoped></style>
