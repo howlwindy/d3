b2ce70d5e28bebc9f8f98a0518b030fa242e589c
howlwindy <howlwindy@gmail.com>
2022-03-02 21:30:22 +0800

    feat(arrays): iterables
    reduce

--- src/models/arrays/iterables/Reduce.vue
+++ src/models/arrays/iterables/Reduce.vue
@@ -1,9 +1,33 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+import SrcVue from '@/components/Src.vue'
+import EgVue from '@/components/Eg.vue'
+import CodeVue from '@/components/Code.vue'
 import GroupItemVue from '@/components/GroupItem.vue'
+import * as d3 from 'd3'
+
+const src = ref<string[]>([
+  'reduce<T>(iterable: Iterable<T>, reducer: (previousValue: T, currentValue: T, currentIndex: number, iterable: Iterable<T>) => T, initialValue?: T): T',
+  'reduce<T, U>(iterable: Iterable<T>, reducer: (previousValue: U, currentValue: T, currentIndex: number, iterable: Iterable<T>) => U, initialValue: U): U'
+])
+
+const data0 = ref<string>('a 2 10 -1 -2')
+const result0 = ref<string>(
+  d3.reduce(data0.value, (prev, next) => prev + next, '')
+)
+const input0 = (v: string) =>
+  (result0.value = d3.reduce(v, (prev, next) => prev + next, ''))
 </script>
 <template>
   <GroupItemVue>
-    <h3><code>reduce</code></h3>
+    <h3><code>reduce - 折叠</code></h3>
+    <SrcVue :data="src"></SrcVue>
+    <EgVue>
+      <input v-model="data0" type="text" @input="input0(data0)" />
+      <CodeVue :result="result0">
+        <code>d3.reduce({{ data0 }}, (prev, next) => prev + next, '')</code>
+      </CodeVue>
+    </EgVue>
   </GroupItemVue>
 </template>
 <style scoped></style>
