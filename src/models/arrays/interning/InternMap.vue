5576b4fc676a60496e94aa05b4d12d767fbff356
howlwindy <howlwindy@gmail.com>
2022-03-02 22:03:34 +0800

    feat(arrays): interning
    InternMap

--- src/models/arrays/interning/InternMap.vue
+++ src/models/arrays/interning/InternMap.vue
@@ -1,9 +1,14 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+import SrcVue from '@/components/Src.vue'
 import GroupItemVue from '@/components/GroupItem.vue'
+
+const src = ref<string[]>(['InternMap<K = any, V = any> extends Map<K, V>'])
 </script>
 <template>
   <GroupItemVue>
-    <h3><code>internMap</code></h3>
+    <h3><code>InternMap</code></h3>
+    <SrcVue :data="src"></SrcVue>
   </GroupItemVue>
 </template>
 <style scoped></style>
