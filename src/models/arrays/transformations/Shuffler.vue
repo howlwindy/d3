b538ba37d65814715e8de1c8143d8e5d785c8d1f
howlwindy <howlwindy@gmail.com>
2022-03-05 22:53:08 +0800

    feat(arrays): transformations
    shuffler

--- src/models/arrays/transformations/Shuffler.vue
+++ src/models/arrays/transformations/Shuffler.vue
@@ -1,9 +1,27 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+import SrcVue from '@/components/Src.vue'
+import EgVue from '@/components/Eg.vue'
+import CodeVue from '@/components/Code.vue'
 import GroupItemVue from '@/components/GroupItem.vue'
+import * as d3 from 'd3'
+
+const src = ref<string[]>(['shuffler(random: () => number): typeof shuffle'])
+
+const data0 = ref<number>(0)
+const result0 = ref<typeof d3.shuffle>(d3.shuffler(() => data0.value))
+const input0 = (v: number) => (result0.value = d3.shuffler(() => v))
 </script>
 <template>
   <GroupItemVue>
-    <h3><code>shuffler</code></h3>
+    <h3><code>shuffler - concat</code></h3>
+    <SrcVue :data="src"></SrcVue>
+    <EgVue>
+      <input v-model="data0" type="number" @input="input0(data0)" />
+      <CodeVue :result="result0">
+        <code>d3.shuffler(() => {{ data0 }})</code>
+      </CodeVue>
+    </EgVue>
   </GroupItemVue>
 </template>
 <style scoped></style>
